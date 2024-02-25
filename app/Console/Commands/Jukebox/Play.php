5e867b1fbba87c3915199207b214a38ac87e92e6
Kate Nikolaeva <ekaterina.nikolaeva@edesk.com>
2024-02-25 20:55:43 +0000

    Play cmd

---
+++ app/Console/Commands/Jukebox/Play.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Console\Commands\Jukebox;
+
+use App\Models\Playlist;
+use App\Models\Track;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+
+class Play extends Command implements PromptsForMissingInput
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'jukebox:play {trackId* : trackID from the list of tracks in the database}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Play a song or add them into playlist';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $trackIds = array_map('intval', $this->argument('trackId'));
+        $tracks = Track::find($trackIds);
+
+        if ($tracks->isEmpty()) {
+            $this->error('No tracks found.');
+            return;
+        }
+
+        $retrievedTrackIds = $tracks->modelKeys();
+
+        $notFound = array_diff($trackIds, $retrievedTrackIds);
+        if (!empty($notFound)) {
+            $this->error('The following track IDs were not found: ' . implode(', ', $notFound));
+        }
+
+        // Tracks in the requested order. There is another way to keep order, but no need to use it here
+        $tracksToAddIds = array_intersect($trackIds, $retrievedTrackIds);
+        $trackToPlay = $tracks->find(current($tracksToAddIds));
+
+        $tracksInQueue = Playlist::whereIn('track_id', $tracksToAddIds)->get();
+        foreach ($tracksInQueue as $trackInQueue) {
+            $this->warn("{$trackInQueue->track->info} is already in the queue.");
+        }
+
+        $existingTrackIds = $tracksInQueue->pluck('track_id')->toArray();
+        $tracksToAddIds = array_diff($tracksToAddIds, $existingTrackIds);
+
+        foreach ($tracks->only($tracksToAddIds) as $track) {
+            //@todo: add track to the queue
+            $this->info("{$track->info} - has been added to the queue.");
+        }
+
+        if (Playlist::startPlaying($trackToPlay)) {
+            $this->info("{$trackToPlay->info} - is now playing.");
+        } else {
+            $this->error("{$trackToPlay->info} - unable to play.");
+        }
+    }
+
+}
--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\DB;
 
 class Playlist extends Model
 {
@@ -24,6 +25,43 @@ class Playlist extends Model
      */
     protected $with = ['track'];
 
+    /**
+     * @param Track $track
+     * Start playing track
+     *
+     * @return bool
+     */
+    public static function startPlaying(Track $track): bool
+    {
+        $result = false;
+        DB::beginTransaction();
+        try {
+            $playlistItem = self::where(['track_id' => $track->id])->first();
+            if (!$playlistItem) {
+                throw new \Exception('Track not found in the queue.');
+            }
+
+            //Need to stop playing all tracks before starting new one
+            self::stopPlaying();
+
+            $playlistItem->playing = true;
+            $result = $playlistItem->save();
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+        }
+        return $result;
+    }
+
+    /**
+     * Stop playing tracks
+     *
+     * @return bool
+     */
+    public static function stopPlaying(): bool
+    {
+        return self::query()->update(['playing' => false]);
+    }
 
     /**
      * @return BelongsTo
